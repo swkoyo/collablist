31881145a2fed37306a4ed108de6d54589f932bd
swkoyo <10441291+swkoyo@users.noreply.github.com>
2022-10-10 16:51:17 +0900

    list update

--- src/types/list.ts
+++ src/types/list.ts
@@ -1,7 +1,21 @@
+import type { User } from './user';
+
 export interface List {
     id: number;
     title: string;
     description: string;
+    created_at: Date;
+    updated_at: Date;
+    items: ListItem[];
+    user: Omit<User, 'created_at' | 'updated_at'>;
+    members: Omit<User, 'created_at' | 'updated_at'>[];
+}
+
+export interface ListItem {
+    id: number;
+    title: string;
+    amount: number;
+    status: boolean;
     created_at: Date;
     updated_at: Date;
 }
