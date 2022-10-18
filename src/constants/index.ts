ee66002bf5aadd38cd94db54c27ad25a4b05582d
swkoyo <10441291+swkoyo@users.noreply.github.com>
2022-10-18 15:10:04 +0900

    using env

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000/v1';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/v1';
 export const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*(\d|[!@#$%^&*]))[A-Za-z\d!@#$%^&*]{8,20}$/;
