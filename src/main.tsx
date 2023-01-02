904865d8b3a545b839c6249a6e38f4277d242818
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-01-02 15:16:18 +0900

    use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import theme from './config/theme';
 import { store } from './redux/store';
@@ -10,11 +10,11 @@ import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ChakraProvider theme={theme}>
-            <BrowserRouter>
+            <HashRouter>
                 <Provider store={store}>
                     <App />
                 </Provider>
-            </BrowserRouter>
+            </HashRouter>
         </ChakraProvider>
     </React.StrictMode>
 );
