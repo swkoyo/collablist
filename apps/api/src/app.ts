70360de5c4f05208fd9ccaf063aac2bd6a6ddbd7
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-21 21:47:27 +0900

    trpc init

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,8 @@
+import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify';
 import fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import { nanoid } from 'nanoid';
+import { appRouter } from './services/trpc';
+import { createContext } from './context';
 
 export function buildApp(logging = true) {
     const app = fastify({
@@ -12,7 +15,13 @@ export function buildApp(logging = true) {
             },
             level: 'debug'
         },
-        genReqId: () => nanoid()
+        genReqId: () => nanoid(),
+        maxParamLength: 5000
+    });
+
+    app.register(fastifyTRPCPlugin, {
+        prefix: '/trpc',
+        trpcOptions: { router: appRouter, createContext }
     });
 
     app.get('/health', (req, res) => {
