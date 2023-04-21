4ff985946ebecdd849a54bf868f64cebdab2e82e
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-21 09:56:56 +0900

    init

--- apps/server/src/config/index.ts
+++ apps/server/src/config/index.ts
@@ -1,6 +1,6 @@
 export default () => ({
     NODE_ENV: process.env.NODE_ENV || 'development',
-    PORT: parseInt(process.env.PORT, 10) || 3000,
+    PORT: parseInt(process.env.SERVER_PORT, 10) || 3000,
     SALT_ROUNDS: parseInt(process.env.BCRYPT_SALT_ROUNDS, 10) || 10,
     JWT: {
         SECRET: process.env.JWT_SECRET || 'shhh',
