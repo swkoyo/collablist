f819c01755282d9c7dd1605dfefd113f30154430
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-21 18:48:24 +0900

    remove nestjs

--- apps/server/src/shared/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export { PASSWORD_REGEX } from '@collablist/shared';
-
-export const USER_ROLE = {
-    ADMIN: 'ADMIN',
-    USER: 'USER'
-};
--- apps/server/src/shared/messages/index.ts
+++
@@ -1,9 +0,0 @@
-export const USER_NOT_FOUND = (value: string | number) =>
-    `User ${value} not found`;
-export const INVALID_CREDENTIALS = 'Invalid credentials';
-export const INVALID_JWT_TOKEN = 'Invalid token';
-export const USER_EXISTS = (value: any) => `User ${value} already exists`;
-export const INVALID_PASSWORD_CONFIRMATION = 'Passwords do not match';
-export const NOT_AUTHORIZED_FOR_LIST = 'You cannot access this list';
-export const USERS_NOT_FOUND = (value: string[] | number[]) =>
-    `Users ${value} not found`;
