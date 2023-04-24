7917d05b421ca39cecb9218e3179e17b558ea12e
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-24 09:59:03 +0900

    Remove post

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,8 @@
 import { authRouter } from "./router/auth";
-import { postRouter } from "./router/post";
 import { taskRouter } from "./router/task";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   auth: authRouter,
   task: taskRouter,
 });
