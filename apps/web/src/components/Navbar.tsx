d0f609023547f6f0144f552b713513fd989a3600
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-25 18:23:00 +0900

    Update navbar and sidebar

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,21 +1,17 @@
-import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
-import {
-  ChevronDownIcon,
-  MagnifyingGlassIcon,
-} from '@heroicons/react/20/solid';
-import { Bars3Icon, BellIcon } from '@heroicons/react/24/outline';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { Bars3Icon } from '@heroicons/react/24/outline';
+import { signOut } from 'next-auth/react';
+import { Fragment } from 'react';
 
+import { api } from '~/utils/api';
 import { classNames } from '~/utils/tailwind';
-
-const userNavigation = [
-  { name: 'Your profile', href: '#' },
-  { name: 'Sign out', href: '#' },
-];
 
 const Navbar: React.FC<{
   openSidebar: () => void;
 }> = ({ openSidebar }) => {
+  const { data: session } = api.auth.getSession.useQuery();
+
   return (
     <div className='sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8'>
       <button
@@ -27,56 +23,31 @@ const Navbar: React.FC<{
         <Bars3Icon className='h-6 w-6' aria-hidden='true' />
       </button>
 
-      {/* Separator */}
-      <div className='h-6 w-px bg-gray-900/10 lg:hidden' aria-hidden='true' />
-
       <div className='flex flex-1 gap-x-4 self-stretch lg:gap-x-6'>
-        <form className='relative flex flex-1' action='#' method='GET'>
-          <label htmlFor='search-field' className='sr-only'>
-            Search
-          </label>
-          <MagnifyingGlassIcon
-            className='pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400'
-            aria-hidden='true'
-          />
-          <input
-            id='search-field'
-            className='block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm'
-            placeholder='Search...'
-            type='search'
-            name='search'
-          />
-        </form>
-        <div className='flex items-center gap-x-4 lg:gap-x-6'>
-          <button
-            type='button'
-            className='-m-2.5 p-2.5 text-gray-400 hover:text-gray-500'
-          >
-            <span className='sr-only'>View notifications</span>
-            <BellIcon className='h-6 w-6' aria-hidden='true' />
-          </button>
-
-          {/* Separator */}
-          <div
-            className='hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10'
-            aria-hidden='true'
-          />
-
+        <div className='ml-auto flex items-center gap-x-4 lg:gap-x-6'>
           {/* Profile dropdown */}
           <Menu as='div' className='relative'>
             <Menu.Button className='-m-1.5 flex items-center p-1.5'>
               <span className='sr-only'>Open user menu</span>
-              <img
-                className='h-8 w-8 rounded-full bg-gray-50'
-                src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                alt=''
-              />
+              {session?.user?.image ? (
+                <img
+                  className='h-8 w-8 rounded-full bg-gray-50'
+                  src={session.user.image}
+                  alt=''
+                />
+              ) : (
+                <span className='inline-flex h-8 w-8 items-center justify-center rounded-full bg-gray-500'>
+                  <span className='text-sm leading-none text-white'>
+                    {session?.user.name ? session.user.name[0] : 'U'}
+                  </span>
+                </span>
+              )}
               <span className='hidden lg:flex lg:items-center'>
                 <span
                   className='ml-4 text-sm font-semibold leading-6 text-gray-900'
                   aria-hidden='true'
                 >
-                  Tom Cook
+                  {session?.user.name}
                 </span>
                 <ChevronDownIcon
                   className='ml-2 h-5 w-5 text-gray-400'
@@ -94,21 +65,19 @@ const Navbar: React.FC<{
               leaveTo='transform opacity-0 scale-95'
             >
               <Menu.Items className='absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none'>
-                {userNavigation.map((item) => (
-                  <Menu.Item key={item.name}>
-                    {({ active }) => (
-                      <a
-                        href={item.href}
-                        className={classNames(
-                          active ? 'bg-gray-50' : '',
-                          'block px-3 py-1 text-sm leading-6 text-gray-900',
-                        )}
-                      >
-                        {item.name}
-                      </a>
-                    )}
-                  </Menu.Item>
-                ))}
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={() => void signOut({ callbackUrl: '/' })}
+                      className={classNames(
+                        active ? 'bg-gray-100' : '',
+                        'block w-full px-3 py-1 text-start text-sm leading-6 text-gray-900',
+                      )}
+                    >
+                      Sign out
+                    </button>
+                  )}
+                </Menu.Item>
               </Menu.Items>
             </Transition>
           </Menu>
--- apps/web/src/components/Sidebar.tsx
+++ apps/web/src/components/Sidebar.tsx
@@ -1,33 +1,17 @@
-import {
-  CalendarIcon,
-  ChartPieIcon,
-  Cog6ToothIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
-} from '@heroicons/react/24/outline';
+import { HomeIcon, TagIcon } from '@heroicons/react/24/outline';
 
 import { classNames } from '~/utils/tailwind';
 
 const navigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
+  { name: 'Tasks', href: '#', icon: HomeIcon, current: true },
+  { name: 'Labels', href: '#', icon: TagIcon, current: false },
 ];
 
 const Sidebar = () => {
   return (
     <div className='flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4'>
-      <div className='flex h-16 shrink-0 items-center'>
-        <img
-          className='h-8 w-auto'
-          src='https://tailwindui.com/img/logos/mark.svg?color=white'
-          alt='Your Company'
-        />
+      <div className='flex h-16 shrink-0 items-center text-2xl font-bold text-white'>
+        <span>na</span>todo
       </div>
       <nav className='flex flex-1 flex-col'>
         <ul role='list' className='flex flex-1 flex-col gap-y-7'>
@@ -58,18 +42,6 @@ const Sidebar = () => {
                 </li>
               ))}
             </ul>
-          </li>
-          <li className='mt-auto'>
-            <a
-              href='#'
-              className='group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-indigo-200 hover:bg-indigo-700 hover:text-white'
-            >
-              <Cog6ToothIcon
-                className='h-6 w-6 shrink-0 text-indigo-200 group-hover:text-white'
-                aria-hidden='true'
-              />
-              Settings
-            </a>
           </li>
         </ul>
       </nav>
