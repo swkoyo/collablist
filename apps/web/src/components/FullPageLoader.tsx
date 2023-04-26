0b1af31d5fdc2ca55e168aacb73a83818713e366
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-26 18:15:10 +0900

    page loader update

--- apps/web/src/components/FullPageLoader.tsx
+++ apps/web/src/components/FullPageLoader.tsx
@@ -1,8 +1,8 @@
 const FullPageLoader = () => {
   return (
-    <div className='absolute bottom-1/2 right-1/2  translate-x-1/2 translate-y-1/2 transform '>
+    <div className='absolute bottom-1/2 right-1/2  translate-x-1/2 translate-y-1/2 transform'>
       <div
-        className='inline-block h-10 w-10 animate-spin rounded-full border-[3px] border-current border-t-transparent text-pink-600'
+        className='inline-block h-10 w-10 animate-spin rounded-full border-[3px] border-current border-t-transparent text-teal-600'
         role='status'
         aria-label='loading'
       >
